dd90ca7cb409c9f879bc9c39d0010effe70b01a9
wuyeelok <wuyeelok@gmail.com>
2025-02-17 16:29:49 +0800

    Build-in checked for overflow

--- Program.cs
+++ Program.cs
@@ -2,7 +2,15 @@
 var part = "Part 7";
 Console.WriteLine($"Hello, World! {part}");
 
+// Version 1
 int a = int.MaxValue;
 int b = 1;
 long c = (long)a + b;
 Console.WriteLine(c);
+
+
+// Version 2
+int d = int.MaxValue;
+int e = 1;
+long f = checked(d + e);
+Console.WriteLine(f);
