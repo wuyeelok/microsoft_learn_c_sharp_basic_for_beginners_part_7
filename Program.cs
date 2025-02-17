00ce886addc00fd921a83837f2a0ac04f6512df5
wuyeelok <wuyeelok@gmail.com>
2025-02-17 15:57:47 +0800

    Part 7 numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var part = "Part 7";
+Console.WriteLine($"Hello, World! {part}");
+
+
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
